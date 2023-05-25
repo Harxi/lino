839be8df50de49f2b3967280999e33d504aa31fa
Harxi <90150354+Harxi@users.noreply.github.com>
2023-05-25 18:25:57 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,19 @@
+"""
+----
+Lino
+----
+
+A simple Pythonic VM
+
+
+From Developer: Don't use for fun!
+"""
+
+__title__ = "Lino"
+__author__ = "HARXI"
+__license__ =  "MIT"
+__copyright__ = "Copyright (c) 2023-present HARXI"
+__version__ = "1.0.0"
+
+from .parser import lexer as lexer
+from . import lino as LinoCore
